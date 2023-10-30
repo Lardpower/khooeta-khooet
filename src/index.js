7e668d8441be85826a192f74184cdae1974cd174
Sasha Reznikov <lard.dev@gmail.com>
2023-10-30 17:55:15 +0100

    Use encapsulated table generator
    Signed-off-by: Sasha Reznikov <lard.dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import {table} from './components'
-import {models} from './models'
-import {dataGenerator} from './dataGenerator'
 import $ from 'jquery'
 import {settingsTable} from './components/settingsTable'
+import {regenerateButton} from './components/regenerateButton'
+import {generateCompanyTable} from './lib/generateCompanyTable'
 
-dataGenerator()
-const companies = models.company.list()
-
-// main table
-const tableContainer = document.getElementById('table-container')
-tableContainer.appendChild(table(companies))
+generateCompanyTable(false)
 
 $(document).ready(function() {
   // place the settings table and all related event handlers
   settingsTable()
 
-  $('#regenerate-companies').click(() => {
-    if (!confirm('Do you really want to regenerate all the companies and users?')){
-      return false;
-    }
-
-    const storage = window.localStorage;
-    storage.removeItem('Company')
-    storage.removeItem('User')
-
-    dataGenerator()
-    const companies = models.company.list()
-    tableContainer.innerHTML = ""
-    tableContainer.appendChild(table(companies))
-  })
+  regenerateButton()
 })
