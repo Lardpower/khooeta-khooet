b69f838a4751793613c6a6b69432d4f5ba46a44c
Sasha Reznikov <lard.dev@gmail.com>
2023-10-30 18:30:09 +0100

    Upser user activity
    Signed-off-by: Sasha Reznikov <lard.dev@gmail.com>

--- src/lib/simulation/runSimulation.js
+++ src/lib/simulation/runSimulation.js
@@ -24,13 +24,28 @@ export const runSimulation = () => {
         Array.from({ length: newCompaniesNum }, () => ({})),
     )
 
-    // exclude some "lost" customers with health score <= 5
+    // exclude some "lost" customers with health score <= 5 (step 3)
     models.company.list().forEach((c) => {
       // chance of losing this customer
       if (Math.random() <= (c.h <= 5 ? 0.3 : 0.05)){
         // consider this client as lost
         models.company.delete(c.id)
       }
+
+      // mark some clients as active (step 6)
+      models.user.list().filter((u) => u.companyId === c.id).forEach((u) => {
+        if (Math.random() <= 0.5){
+          const currentUsers = models.user.list();
+          for(let i = 0; i <= currentUsers.length; i++){
+            if (currentUsers[i].id === u.id){
+              currentUsers[i].lastActive = new Date();
+            }
+          }
+
+          // update current users
+          models.user.update(currentUsers)
+        }
+      })
     })
 
     rebuildCompanyTable()
