07a4046bb760a01c94e058d5ecb68dd7f61489ca
Sasha Reznikov <lard.dev@gmail.com>
2023-10-30 16:00:44 +0100

    Add three new init companies
    Signed-off-by: Sasha Reznikov <lard.dev@gmail.com>

--- src/dataGenerator.js
+++ src/dataGenerator.js
@@ -5,6 +5,9 @@ const initCompanies = [
   {id:2, name: 'Google', h: 5},
   {id:3, name: 'Spotify', h: 10},
   {id:4, name: 'Klarna', h: 3},
+  {id:5, name: 'Uber', h: 5},
+  {id:6, name: 'Microsoft', h: 7},
+  {id:6, name: 'Netflix', h: 3},
 ]
 export function dataGenerator() {
   _.each(initCompanies, c => {
