e151880f5c1df5ccb9ea0f4a39cb5d37e3428e34
Sasha Reznikov <lard.dev@gmail.com>
2023-10-30 16:09:23 +0100

    Add three new fields to Company model
    The field "health" is skipped because it's already
    been there.
    
    Signed-off-by: Sasha Reznikov <lard.dev@gmail.com>

--- src/models/company.js
+++ src/models/company.js
@@ -3,17 +3,20 @@ import {GenericModel} from './generic'
 const properties = [
   { id: 'id', name: 'Id', type: 'number', required: true },
   { id: 'name', name: 'Name', type: 'string', required: true },
+  { id: 'value', name: 'Value', type: 'number' },
   { id: 'h', name: 'health', type: 'number' },
+  { id: 'userCount', name: 'User count', type: 'number' },
+  { id: 'industry', name: 'Industry', type: 'string' },
   { id: 'dateCreated', name: 'Created', type: 'date' },
   { id: 'dateUpdated', name: 'Modified', type: 'date' },
 ]
 
 class Company extends GenericModel {
   constructor() {
-    super();
-    this.properties = properties;
-    this.name = 'Company';
+    super()
+    this.properties = properties
+    this.name = 'Company'
   }
 }
 
-export const company = new Company();
+export const company = new Company()
